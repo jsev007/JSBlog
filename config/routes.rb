e8e881b29acdf446040f09e0836084fcbe0e0ddc
Jorge Sevilla <jorgesevilla7@gmail.com>
2018-08-12 21:24:05 -0700

    Added user-signup, user show with gravatar and articles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :users
+  resources :users, except: [:new]
   resources :articles
   root 'pages#home'
 
   get 'about', to: 'pages#about'
-  
+  get 'signup', to: 'users#new'
 end
